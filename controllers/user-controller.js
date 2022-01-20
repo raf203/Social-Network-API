c23bf2f498d5c581ee909343c1c293e0157d5433
Rafael Cardoso <rrafa203@gmail.com>
2022-01-20 12:59:58 -0800

    set up user-controller and user model

---
+++ controllers/user-controller.js
@@ -0,0 +1,109 @@
+const { User } = require("../models");
+
+
+const userController = {
+   //get all users
+    getAllUsers(req, res) {
+      User.find({})
+        .sort({ _id: -1 })
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+
+    //get user by id 
+    getUserById({ params }, res) {
+      User.findOne({ _id: params.userId })
+        .select('-__v')
+        .populate({
+          path: "friends",
+        })
+        .populate({
+            path: "thoughts",
+        })
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "User not found." });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+
+  //post  user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //update user by _id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "User not found." });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
+  //delete user by _id
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "User not found." });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
+  //post friend connection
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "User not found." });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //delete friend connection
+  removeFriend({ params }, res) {
+    console.log("remove friend", params.friendId);
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = userController;
